ecb48c3a534723b54e57b6470858e5fc58ce1766
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-02-02 11:12:12 +0100

    Add 25519 DH functions

---
+++ crypto/dh.go
@@ -0,0 +1,48 @@
+package crypto
+
+import (
+	"crypto/rand"
+	"errors"
+	"golang.org/x/crypto/curve25519"
+)
+
+// specifying the size in bytes of public keys and DH output
+const DHLEN = 32
+
+/*
+ * KeyPair struct for holding private and public keys
+ */
+type KeyPair struct {
+	PrivateKey [32]byte
+	PublicKey  [32]byte
+}
+
+/*
+ * GenerateKeypair generates a new keypair
+ */
+func GenerateKeypair() (*KeyPair, error) {
+	var keyPair KeyPair
+
+	if _, err := rand.Read(keyPair.PrivateKey[:]); err != nil {
+		err = errors.New("Error generating keypair")
+		return nil, err
+	}
+
+	curve25519.ScalarBaseMult(&keyPair.PublicKey, &keyPair.PrivateKey)
+
+	return &keyPair, nil
+}
+
+/*
+ * DH performs a Diffie-Hellman key exchange and returns the shared key
+ */
+func DH(privateKey, publicKey []byte) ([]byte, error) {
+	sharedKey, err := curve25519.X25519(privateKey, publicKey)
+	if err != nil {
+		err = errors.New("Error generating shared key")
+		return nil, err
+	}
+
+	return sharedKey, err
+}
+
