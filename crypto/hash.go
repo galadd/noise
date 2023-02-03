1387aeb50f0f9e589628e146c7d3edc6e2deba23
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-02-03 06:47:19 +0100

    Add instruction amd edit Hash

--- crypto/hash.go
+++ crypto/hash.go
@@ -6,16 +6,26 @@ import (
 	"errors"
 )
 
+/*
+ * Hashlen is the length of the hash output.
+ * Blocklen is the length of the block input.
+ */
 const (
 	Hashlen = 32
 	Blocklen = 64
 )
 
-func Hash(data []byte) []byte {
+/*
+ * Hash hashes the data with SHA256.
+ */
+func Hash(data []byte) [Hashlen]byte {
 	hash := sha256.Sum256(data)
-	return hash[:]
+	return hash
 }
 
+/*
+ * HashHMAC performs the HMAC-SHA256 function.
+ */
 func HashHMAC(key, data []byte) []byte {
 	hash := sha256.New
 	mac := hmac.New(hash, key)
@@ -23,10 +33,12 @@ func HashHMAC(key, data []byte) []byte {
 	return mac.Sum(nil)
 }
 
-func Hkdf(chainingKey, inputKeyMaterial []byte, numOutputs uint8) ([][][]byte, error) {
-
+/*
+ * Hkdf performs the HKDF key derivation function.
+ */
+func Hkdf(chainingKey, inputKeyMaterial []byte, numOutputs uint8) ([]byte, []byte, []byte, error) {
 	if numOutputs != 2 && numOutputs != 3 {
-		return nil, errors.New("numOutputs must be 2 or 3")
+		return nil, nil, nil, errors.New("numOutputs must be 2 or 3")
 	}
 
 	tempKey := HashHMAC(chainingKey, inputKeyMaterial)
@@ -34,9 +46,9 @@ func Hkdf(chainingKey, inputKeyMaterial []byte, numOutputs uint8) ([][][]byte, e
 	output2 := HashHMAC(tempKey, append(output1, 2))
 
 	if numOutputs == 2 {
-		return [][][]byte{[][]byte{output1, output2}}, nil
+		return output1, output2, nil, nil
 	} else {
 		output3 := HashHMAC(tempKey, append(output2, 3))
-		return [][][]byte{[][]byte{output1, output2, output3}}, nil
+		return output1, output2, output3, nil
 	}
 }
